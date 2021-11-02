d7c6c91726cdbd6f3453ca2507066ade92c61edb
kris <wilson.kristen614@gmail.com>
2021-11-02 12:30:23 -0500

    test

--- src/main/java/com/codeup/springblog/controllers/PostControllers.java
+++ src/main/java/com/codeup/springblog/controllers/PostControllers.java
@@ -1,5 +1,10 @@
 package com.codeup.springblog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class PostControllers {
 //    This controller should return strings for the following routes that describe what will ultimately be there.
 //method	url	description
@@ -7,6 +12,15 @@ public class PostControllers {
 //GET	/posts/{id}	view an individual post
 //GET	/posts/create	view the form for creating a post
 //POST	/posts/create	create a new post
+  @GetMapping("/posts")
+    @ResponseBody
+    public String index(){
+      return "Here are all the posts...";
+  }
+
+  @GetMapping("/posts/{id}")
+    @ResponseBody
+    public String
 
 
 
